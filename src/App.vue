eea84aac030648c8c90e1caa325978922a469460
cecep <cecepjanuardi31@egmail.com>
2022-09-08 16:45:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {RouterView } from 'vue-router'
+import NavbarMenu from './components/NavbarMenu.vue';
+
+</script>
+
+<template>
+  <header>
+     <NavbarMenu />
+  </header>
+
+  <RouterView class="px-2" />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="You did it!" />
+
+    <TheWelcome />
+  </main>
+</template>
